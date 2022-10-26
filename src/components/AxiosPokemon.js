e7304baf122891abb376455e21c2178fc57ea53e
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-26 10:51:32 +0200

    Interpolation

--- src/components/AxiosPokemon.js
+++ src/components/AxiosPokemon.js
@@ -1,17 +1,26 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import { toContainHTML } from '@testing-library/jest-dom/dist/matchers'
 
 const AxiosPokemon = () => {
     const [pokemons, setPokemons] = useState()
+    const [limit, setLimit] = useState(10)
 
     useEffect(()=>{
-        axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=10')
+        axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=${limit}`)
             .then(response => {setPokemons(response.data.results)})
-    },[])
+    },[limit])
+
+    const handleClick = (e) => {
+        setLimit(e.target.innerHTML)
+    }
 
   return (
     <div>
         <h2>Axios Example</h2>
+        <button onClick={handleClick}>10</button>
+        <button onClick={handleClick}>20</button>
+        <button onClick={handleClick}>30</button>
 
         {
             pokemons ?
